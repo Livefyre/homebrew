a5a81565ec2d68dfded26f1e8e2bc76c57350450
Nino Walker <nino.walker@gmail.com>
2012-09-17 10:45:37 -0700

    Setup for selenium etc.

---
+++ Library/Formula/lfservices.rb
@@ -0,0 +1,95 @@
+require 'formula'
+
+class Lfservices < Formula
+  homepage 'http://github.com/Livefyre/lfdj'
+  #head 'git@github.com:Livefyre/lfdj.git'
+  url 'https://raw.github.com/gist/7181e7f98f07ca234595/e81cb0a56c2e82cf88efe77dcb49ee4028193c5b/supervisord.conf'
+  version '1.0'
+  depends_on 'mysql'
+  depends_on 'mongodb'
+  depends_on 'redis'
+  depends_on 'elasticsearch'
+  depends_on 'supervisord'
+  depends_on 'selenium-server-standalone'
+
+  def install
+    (etc + 'supervisor/conf.d/lfservices').mkpath
+    (etc + 'supervisor/conf.d/lfservices/es.conf').write elasticsearch
+    (etc + 'supervisor/conf.d/lfservices/redis.conf').write redis
+    (etc + 'supervisor/conf.d/lfservices/mongo.conf').write mongo
+    (etc + 'supervisor/conf.d/lfservices/mysql.conf').write mysql
+    (etc + 'supervisor/conf.d/lfservices/selenium.conf').write selenium
+  end
+
+  def selenium
+    return <<-EOS
+[program:selenium]
+command = /usr/bin/java -jar /usr/local/Cellar/selenium-server-standalone/2.25.0/selenium-server-standalone-2.25.0.jar -port 4444
+process_name = selenium
+directory = /usr/local/var
+priority = 5
+autorestart = false
+autostart = false
+startsecs = 5
+startretries = 10
+user = #{ENV['USER']}
+    EOS
+  end
+
+  def mysql
+    return <<-EOS
+[program:mysql]
+command = /usr/local/bin/mysqld_safe
+process_name = mysql
+directory = /usr/local/var
+priority = 5
+autorestart = true
+startsecs = 5
+startretries = 10
+user = #{ENV['USER']}
+    EOS
+  end
+
+  def mongo
+    return <<-EOS
+[program:mongo]
+command = /usr/local/opt/mongodb/mongod run --config /usr/local/etc/mongod.conf
+process_name = mongo
+directory = /usr/local/var
+priority = 5
+autorestart = true
+startsecs = 5
+startretries = 10
+user = #{ENV['USER']}
+    EOS
+  end
+
+  def redis
+    return <<-EOS
+[program:redis]
+command = /usr/local/bin/redis-server /usr/local/etc/redis.conf
+process_name = redis
+directory = /usr/local/var
+priority = 5
+autorestart = true
+startsecs = 5
+startretries = 10
+user = #{ENV['USER']}
+    EOS
+  end
+
+  def elasticsearch
+    return <<-EOS
+[program:elasticsearch]
+command = /usr/local/bin/elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.19.9/config/elasticsearch.yml
+process_name = elasticsearch
+directory = /usr/local/var
+priority = 5
+autorestart = true
+startsecs = 5
+startretries = 10
+environment=ES_JAVA_OPTS=-Xss200000
+user = #{ENV['USER']}
+    EOS
+  end
+end
