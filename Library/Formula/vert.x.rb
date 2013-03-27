6e077e5679511ca7f3346c3ad346b7b8c8b6550d
Jonathan Klaassen <jonathan@livefyre.com>
2013-03-27 14:37:07 -0700

    Fixing file checksum.

--- Library/Formula/vert.x.rb
+++ Library/Formula/vert.x.rb
@@ -4,7 +4,7 @@ class VertX < Formula
   homepage 'http://vertx.io'
   url 'http://packages.livefyre.com/buildout/packages/vert.x-1.3.1.final.tar.gz'
   version '1.3.1'
-  sha1 '734358191245baf63047264008867e69e7d56bed'
+  sha1 '166bf64048ad18abafc305ebce9ee623aca88342'
   depends_on "java7"
   depends_on "jython"
 
