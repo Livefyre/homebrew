fd4a973c40256037208050762f0e401f09391f36
Jonathan Klaaassen <jaklaassen@gmail.com>
2013-04-02 16:44:53 -0700

    Add busmod formula.

---
+++ Library/Formula/vert.x-busmod-amqp.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class VertXBusmodAmqp < Formula
+  homepage 'https://github.com/jaklaassen/voom-java'
+  url 'http://packages.livefyre.com/buildout/packages/amqp-busmod-1.2.0-SNAPSHOT-jar-with-dependencies.jar'
+  version '1.2.0'
+  sha1 'a57fba1cfc9a221d9c088467fe3223b42ff10a2c'
+  depends_on "vert.x"
+
+  def install
+     prefix.install Dir['*']
+     system "mvn install:install-file -Dfile=#{prefix}/amqp-busmod-1.2.0-SNAPSHOT-jar-with-dependencies.jar -DgroupId=org.vert-x.mod -DartifactId=amqp-busmod -Dversion=1.2.0-SNAPSHOT -Dpackaging=jar"
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test vert.x`.
+    #system "false"
+  end
+end
