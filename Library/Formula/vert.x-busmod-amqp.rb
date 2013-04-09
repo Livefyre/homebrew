2f6b88f7549fa2dc56bc05551b5b1e2cf1d738ae
Jonathan Klaaassen <jaklaassen@gmail.com>
2013-04-08 18:53:00 -0700

    Change voom-java and amqp busmod to use git download strategy.

--- Library/Formula/vert.x-busmod-amqp.rb
+++ Library/Formula/vert.x-busmod-amqp.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class VertXBusmodAmqp < Formula
   homepage 'https://github.com/jaklaassen/voom-java'
-  url 'http://packages.livefyre.com/buildout/packages/amqp-busmod-1.2.0-SNAPSHOT-jar-with-dependencies.jar'
+  url 'git@github.com:jaklaassen/vert.x-busmod-amqp.git', :using => GitDownloadStrategy
   version '1.2.0'
-  sha1 'a57fba1cfc9a221d9c088467fe3223b42ff10a2c'
   depends_on "vert.x"
+  depends_on "voom-java"
 
   def install
      prefix.install Dir['*']
-     system "mvn install:install-file -Dfile=#{prefix}/amqp-busmod-1.2.0-SNAPSHOT-jar-with-dependencies.jar -DgroupId=org.vert-x.mod -DartifactId=amqp-busmod -Dversion=1.2.0-SNAPSHOT -Dpackaging=jar"
+     system "cd #{prefix} && mvn install"
   end
 
   def test
